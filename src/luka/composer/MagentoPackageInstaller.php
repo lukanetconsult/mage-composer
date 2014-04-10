36396803bd43aa11de33491bc2d8e420378989b1
tux-rampage <staff@pro-unreal.de>
2014-04-10 16:55:18 +0200

    Initial commit

--- src/luka/composer/MagentoPackageInstaller.php
+++ src/luka/composer/MagentoPackageInstaller.php
@@ -180,7 +180,7 @@ class MagentoPackageInstaller extends LibraryInstaller
 
             if (!isset($archive[$path])) {
                 if (!isset($archive[$targetPath])) {
-                    $this->io->write('<error>Failed to extract package file: ' . $path . '<error>');
+                    $this->io->write('    <error>Failed to extract package file: ' . $path . '<error>');
                     continue;
                 }
 
@@ -188,7 +188,7 @@ class MagentoPackageInstaller extends LibraryInstaller
             }
 
             if (file_exists($targetPath) && !$overwrite) {
-                $this->io->write('<warning>Cannot install package file (File exists): ' . $targetPath . '<warning>');
+                $this->io->write('    <warning>Cannot install package file (File exists): ' . $targetPath . '<warning>');
                 continue;
             }
 
